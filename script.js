b8ab2fa065de7cc8f7010d256d0fcfde1e656453
whishad <coderlweer@gmail.com>
2023-02-28 00:31:52 +0400

    creating constructor PicProperties and giving him properties width,hei...

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 const canvas = document.querySelector('canvas')
 const ctxt = canvas.getContext('2d')
+
+function PicProperties(width, height, rect_fill, back_fill){
+    this.width = width
+    this.height = height
+    this.rect_fill = rect_fill
+    this.back_fill = back_fill
+}
